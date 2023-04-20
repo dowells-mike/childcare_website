60ee40a0648be306f8ceb25ff7837ea9cf6fd94a
AiTheOne <109893753+dowells-mike@users.noreply.github.com>
2023-04-20 15:05:53 +0100

    created a new repository, and added main pages

---
+++ header.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Childcare Premises</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="testimonial.php">Testimonials</a></li>
+                <li><a href="contact_us.php">Contact Us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <?php if(isset($_SESSION['parent_id'])) { ?>
+                    <li><a href="parent_dashboard.php">Dashboard</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php } elseif(isset($_SESSION['admin_id'])) { ?>
+                    <li><a href="admin_dashboard.php">Dashboard</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php } else { ?>
+                    <li><a href="parent_login.php">Parent Login</a></li>
+                    <li><a href="admin_login.php">Admin Login</a></li>
+                <?php } ?>
+            </ul>
+            <form class="navbar-form navbar-right" action="search.php" method="get">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search" name="q">
+                </div>
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+            </form>
+            <?php if(isset($_SESSION['parent_id'])) { ?>
+                <p class="navbar-text navbar-right">Logged in as Parent</p>
+            <?php } elseif(isset($_SESSION['admin_id'])) { ?>
+                <p class="navbar-text navbar-right">Logged in as Admin</p>
+            <?php } ?>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Childcare Premises</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+
+    <div class="jumbotron text-center">
+        <h1>Welcome to Childcare Premises</h1>
+        <p>Providing a safe, nurturing, and stimulating environment for young children.</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="thumbnail">
+                    <img src="images/feature1.jpg" alt="Feature 1">
+                    <div class="caption">
+                        <h3>New Activities</h3>
+                        <p>Check out the latest activities that we have for your kids. They will surely love it!</p>
+                        <a href="activities.php" class="btn btn-primary" role="button">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="thumbnail">
+                    <img src="images/feature2.jpg" alt="Feature 2">
+                    <div class="caption">
+                        <h3>Special Offers</h3>
+                        <p>We have a range of special offers available for new and existing customers. Don't miss out!</p>
+                        <a href="offers.php" class="btn btn-primary" role="button">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
