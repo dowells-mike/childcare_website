232eb6488f4aa4a2267af5c4188b86d9955429a3
AiTheOne <109893753+dowells-mike@users.noreply.github.com>
2023-05-02 21:04:45 +0100

    used bootstrap in styling of contact us manage page

--- contact_us_manage.php
+++ contact_us_manage.php
@@ -1,39 +1,113 @@
 <?php
 session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+/*if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
     header('Location: index.php');
     exit();
 }
+*/
 
 require_once '../../connection.php';
 
 $sql = "SELECT * FROM contact_us_message";
 $result = $db_connection->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "<table class='table'>";
-    echo "<thead>";
-    echo "<tr>";
-    echo "<th>Name</th>";
-    echo "<th>Email</th>";
-    echo "<th>Phone Number</th>";
-    echo "<th>Message</th>";
-    echo "</tr>";
-    echo "</thead>";
-    echo "<tbody>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row['name'] . "</td>";
-        echo "<td>" . $row['email'] . "</td>";
-        echo "<td>" . $row['phone_number'] . "</td>";
-        echo "<td>" . $row['message'] . "</td>";
-        echo "</tr>";
-    }
-    echo "</tbody>";
-    echo "</table>";
-} else {
-    echo "No messages found.";
-}
-
-$db_connection->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us Messages</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Childcare Premises</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="services.php">Services and Facilities</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="testimonial.php">Testimonials</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact_us.php">Contact Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <?php if (isset($_SESSION['user_level'])): ?>
+                    <li class="nav-item">
+                        <span class="navbar-text">Welcome,
+                            <?php echo $_SESSION['username']; ?>!
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </nav>
+
+    <!--Table of messages-->
+    <div class="container">
+        <h1>Message Inbox</h1>
+
+        <table class="table table-dark">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th>Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = $result->fetch_assoc()) { ?>
+                    <tr>
+                        <td>
+                            <?php echo $row['name']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['email']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['phone_number']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['message']; ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
