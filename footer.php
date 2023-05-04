6b22fc32c3b4cc7249e349c200afa9a5ef2a029f
AiTheOne <109893753+dowells-mike@users.noreply.github.com>
2023-05-04 22:14:54 +0100

    added footer and its corresponding assets

--- footer.php
+++ footer.php
@@ -1,3 +1,31 @@
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-sm-6 col-md-4 footer-navigation">
+            <h3><a href="#">Cozy<span>Cubs </span></a></h3>
+            <p class="links"><a href="index.php">Home</a><strong> · </strong><a href="Contact_us.php">Contact Us</a></p>
+            <p class="company-name">Cozy Cubs © 2023 </p>
+        </div>
+        <div class="col-sm-6 col-md-4 footer-contacts">
+            <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                <p><span class="new-line-span">S Circular Rd, Saint Catherine's, Dublin 8, D08 V04N</span> Dublin, Ireland</p>
+            </div>
+            <div><i class="fa fa-phone footer-contacts-icon"></i>
+                <p class="footer-center-info email text-start"> +353 8949 87944</p>
+            </div>
+            <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                <p><a href="#" target="_blank">childcareswd@googlegroups.com</a></p>
+            </div>
+        </div>
+        <div class="col-md-4 footer-about">
+            <h4>About our organisation</h4>
+            <p> Our company is dedicated to providing high-quality childcare services to families. We strive to create a nurturing and stimulating environment for children to learn and grow in </p>
+            <div class="social-links social-icons"><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a><a href="https://ie.linkedin.com/"><i class="fa fa-linkedin"></i></a><a href="https://github.com/"><i class="fa fa-github"></i></a></div>
+        </div>
+    </div>
+</footer>
+
+
 <!-- Bootstrap JavaScript -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
@@ -6,6 +34,9 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
     integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
     crossorigin="anonymous"></script>
+
+<script src="/assets-footer/bootstrap/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
